742f4ab4f8bd52aed0d0c10eff802a36dd271828
nelsonabieno <=>
2020-02-17 11:53:14 +0100

    add functionality to search github

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,6 +1,11 @@
 class RepoController < ApplicationController
   def index
-    github = Github.new
-
+    if params[:repo].present?
+      base_uri= "https://api.github.com/search/repositories?q=#{params[:repo]}+in%3Aname&type=Repositories"
+      @res = HTTParty.get(base_uri)
+      @response= JSON.parse(@res.body)
+    else
+      @message = "We couldn't find what you were looking for"
+    end
   end
 end
