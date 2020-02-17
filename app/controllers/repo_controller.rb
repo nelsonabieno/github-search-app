1a208ab404bb606668dca7b2620f52188c70e725
nelsonabieno <=>
2020-02-17 11:54:56 +0100

    add route and view files

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -2,8 +2,8 @@ class RepoController < ApplicationController
   def index
     if params[:repo].present?
       base_uri= "https://api.github.com/search/repositories?q=#{params[:repo]}+in%3Aname&type=Repositories"
-      @res = HTTParty.get(base_uri)
-      @response= JSON.parse(@res.body)
+      @response = HTTParty.get(base_uri)
+      @parsed_response= JSON.parse(@response.body)
     else
       @message = "We couldn't find what you were looking for"
     end
